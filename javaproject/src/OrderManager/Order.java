711ddca7ac8a8ff222cdef3df37b9e3cbd8f114a
Harry <hgraham594@gmail.com>
2018-02-23 08:13:01 +0000

    Merged two locations
    Merged from separate locations to stay up to date.
    
    No big changes introduced here.

--- javaproject/src/OrderManager/Order.java
+++ javaproject/src/OrderManager/Order.java
@@ -5,22 +5,22 @@ import java.util.ArrayList;
 import Ref.Instrument;
 
 public class Order implements Serializable {
-	public long transactionID;
+	public int transactionID; 						//TODO these should all be longs
 	public int clientOrderID;
-    long clientID;
+    int clientID;
 //	short orderRouter;
 	private int size;
 	double[] bestPrices;
 	int bestPriceCount;
     public Instrument instrument;
     public double initialMarketPrice;
-	ArrayList<Order> slices;
+    ArrayList<Order> slices;
     private ArrayList<Fill> fills;
     char OrdStatus = 'A';                 //OrdStatus is Fix 39, 'A' is 'Pending New'
     //Status state;
 
     // The constructor for a new order.
-    public Order(long clientId, int ClientOrderID, Instrument instrument, int size) {
+    public Order(int clientId, int ClientOrderID, Instrument instrument, int size) {
         this.clientOrderID = ClientOrderID;
         this.size = size;
         this.clientID = clientId;
